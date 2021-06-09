f720bcce0157fa102580c061910a4c82c76dbcdf
pme1123 <pmewing87@gmail.com>
2021-06-08 23:15:45 -0500

    begin spatial ggplot of heritability map

--- R/figure code/Fig 3 - Heritability across space, time.R
+++ R/figure code/Fig 3 - Heritability across space, time.R
@@ -6,9 +6,64 @@ library(ggplot2)
 library(ggpmisc)
 library(gridExtra)
 library(wesanderson)
+library(stars)
+library(sf)
 
 popn = 'Race'
 colors = wes_palette('FantasticFox1')
+
+in_dir = file.path('results', 'figure data')
+
+sites  = here(in_dir, 'Figure 1 - Yield by Site.gpkg') %>% 
+  st_read
+basemap =   here(in_dir, 'CDBN Basemap.gpkg') %>% 
+  st_read
+H_map = here(in_dir, 'test_kriged_bean_map.tif') %>%
+  read_stars(proxy=FALSE)
+st_crs(H_map) = "epsg:4326"
+
+## Figure 3a: Heritability across space
+# buffer to crop
+site_buff = sites %>% 
+  st_union %>% 
+  st_buffer(100000) %>% 
+  st_convex_hull %>% 
+  st_transform(st_crs(H_map))
+
+H_map %<>%
+  # st_warp(cellsize=0.1,
+  #         method='bilinear',
+  #         use_gdal=TRUE) %>%
+  st_crop(site_buff)# %>% 
+  # st_transform(st_crs(basemap))
+basemap %<>% st_transform(st_crs(H_map))
+
+# st_as_sf %>% 
+# set_colnames(c('value', 'geometry'))
+
+H_space = ggplot(basemap) +
+  scale_fill_gradientn(colors=colors[c(3,2,5)]) +
+  geom_sf(fill='white',
+          size=0.2) +
+  geom_stars(data=H_map,
+             alpha=0.8) +
+  geom_sf(data=sites,
+          color='black',
+          size=0.2) +
+  labs(fill='Yield\nHeritability') +
+  theme_minimal() +
+  theme(axis.ticks=element_line(color='gray',
+                                size=0.2),
+        axis.title=element_text(size=9),
+        legend.title=element_text(size=9),
+        panel.grid=element_line(color='gray',
+                                size=0.2)) +
+  ggtitle('A)') +
+  theme(plot.title=element_text(size=10,
+                                hjust=-0.38))
+H_space
+
+## Figure 3b: Heritability across time
 
 se = function(x) sd(x) / sqrt(length(x))
 se_high = function(x) mean(x) + se(x)
@@ -59,15 +114,16 @@ H_yr = ggplot(H_df,
   theme(panel.grid=element_blank(),
         axis.ticks=element_line(color='gray',
                                 size=0.2),
-        axis.title=element_text(size=9))
-
-H_yr = H_yr + 
+        axis.title=element_text(size=9)) +
   ggtitle('B)') + 
   theme(plot.title=element_text(size=10))
+H_yr
 
 
-"Figure 3b - Heritability vs Time.jpg" %>% 
+"Figure 3 - Heritability across Space, Time.jpg" %>% 
   here('results', .) %>% 
-  jpeg(width=6.5, height=2.5, units='in', res=300)
-grid.arrange(H_yr)
+  jpeg(width=6.5, height=5, units='in', res=300)
+grid.arrange(grobs=list(H_space,
+                        H_yr),
+             heights=c(5, 4))
 dev.off()
